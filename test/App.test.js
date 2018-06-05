73bf12c63f6eccc2907565033adba65c50555dcf
Matheus Almeida <mat.almeida@live.com>
2018-06-04 23:07:03 -0300

    Added first test

---
+++ test/App.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import App from "../src/components/App";
+
+describe("App", () => {
+  test("should match snapshot", () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find("h1").text()).toBe("Welcome to my Boilerplate");
+    expect(wrapper).toMatchSnapshot;
+  });
+});
